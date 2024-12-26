636931e7a6d3f6baf819f624ed379c853265c11b
Clifford Mulenga <clifford.c.mulenga@gmail.com>
2024-12-26 21:09:19 +0200

    add the special section slider

---
+++ js/swiperSilder.js
@@ -0,0 +1,34 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 2,
+    spaceBetween: 40,
+    loop: true,
+    autoPlay: {
+      delay: 3000,
+      disableOnInteraction: false
+    },
+   
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 1,
+      },
+      950: {
+        slidesPerView: 2,
+      },
+    },
+  });
