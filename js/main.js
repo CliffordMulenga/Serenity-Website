14314a9b4b3163726b31b0b957729d9f34fe8a5e
Clifford Mulenga <clifford.c.mulenga@gmail.com>
2024-12-28 05:30:23 +0200

    added tap and hover to watch video

--- js/main.js
+++ js/main.js
@@ -1,19 +1,24 @@
-// specail package play video on hover
-// Select all video cards
+// Select all cards with videos
 const videoCards = document.querySelectorAll('.swiper-slide .card');
 
 videoCards.forEach(card => {
     const video = card.querySelector('.cardVideo');
-    
-    // Play video on hover
+    const videoTag = card.querySelector('.watchVideo');
+
+    // Play video and hide tag on hover or tap
     card.addEventListener('mouseover', () => {
         video.play();
+        videoTag.style.opacity = "0";
+        videoTag.style.visibility = "hidden";
     });
-    
-    // Pause video when hover ends
+
+    // Pause video and show tag on hover out
     card.addEventListener('mouseout', () => {
         video.pause();
         video.currentTime = 0;
-        video.load(); // reloads video so that the poster reappears
+        video.load();
+        videoTag.style.opacity = "1";
+        videoTag.style.visibility = "visible";
     });
+
 });
